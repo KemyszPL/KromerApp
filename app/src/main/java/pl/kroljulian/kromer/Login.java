781c3d3944b220e145a82ad9fbd62b0d968ced48
KemyszPL <malpkaszymix@gmail.com>
2022-07-24 22:55:01 +0200

    I need to stop breaking versioncontrol

---
+++ app/src/main/java/pl/kroljulian/kromer/Login.java
@@ -0,0 +1,14 @@
+package pl.kroljulian.kromer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
