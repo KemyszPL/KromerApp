150885162980024c9e9eec464faee9dbd3f1a5df
KemyszPL <malpkaszymix@gmail.com>
2022-07-28 15:53:11 +0200

    Initiate Account View

---
+++ app/src/main/java/pl/kroljulian/kromer/AccountView.java
@@ -0,0 +1,14 @@
+package pl.kroljulian.kromer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AccountView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_view);
+    }
+}
